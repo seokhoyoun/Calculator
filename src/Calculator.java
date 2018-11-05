27dac3dc8050dcba4f669e4d05cae86452e9ae3c
Youn <Youn@210.100.148.60>
2018-11-05 17:28:03 +0900

    사칙연산 기호에 따라 다르게 실행되는 계산기

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,13 +12,23 @@ public class Calculator {
 		System.out.print("두 번째 값을 입력하세요 : ");
 		int second = scanner.nextInt();
 		
-		System.out.println("두 번째 값 : "+ second+ "\n");
+		System.out.println("두 번째 값 : "+ second);
 		
+		System.out.print(" +, -, *, / 중 하나를 선택하세요 : ");
 		
+		String symbol = scanner.next();
 		
-		System.out.println("덧셈 결과 : "+ (first+second));
-		System.out.println("뺄셈 결과 : "+ (first-second));
-		System.out.println("곱셈 결과 : "+ (first*second));
-		System.out.println("나누기 결과 : "+ (first/second));
+		if("+".equals(symbol))
+			System.out.println("덧셈 결과 : "+ (first+second));
+		else if("-".equals(symbol))	
+			System.out.println("뺄셈 결과 : "+ (first-second));
+		else if("*".equals(symbol))	
+			System.out.println("곱셈 결과 : "+ (first*second));
+		else if("/".equals(symbol))
+			System.out.println("나누기 결과 : "+ (first/second));
+		else
+			System.out.println("사용할 수 없는 연산기호입니다.");
+			
+		
 	}
 }
