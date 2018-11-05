7115610b26dc728e205b340e9979a0baeeb43f3b
Youn <Youn@210.100.148.60>
2018-11-05 16:04:20 +0900

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator {
+	public static void main(String [] args) {
+		
+		System.out.println(3+3);
+		System.out.println(3-3);
+		System.out.println(3*3);
+		System.out.println(3/3);
+	}
+}
