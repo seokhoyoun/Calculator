d98169a20f22cebfc741f45f978088e56e218c84
Youn <Youn@210.100.148.60>
2018-11-09 00:01:00 +0900

    버그 수정

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class StringCalculator {
 	public static void main(String[] args) {
-		System.out.print("값을 입력하세요 : " );
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("값을 입력하세요 : " );
 		String value = scanner.nextLine();
-		
-		System.out.print("입력 값 : " + value);
 		String[] values = value.split(" "); 
+		
+		System.out.println("입력 값 : " + value);
 		int first = Integer.parseInt(values[0]);
-		int result = 0;
+		int result = first;
 		int i = 1;
 		
 				while(i<values.length) {
@@ -18,6 +18,7 @@ public class StringCalculator {
 				result = Calculator.calculate(result, second, symbol);
 				i = i + 2;
 				}
+				Output.print(result);
 			
 	}
 				
